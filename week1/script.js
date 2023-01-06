b67e63da0f5931da2dae3187bf929bad559e7947
Emily-is-cool <79616525+Emily-is-cool@users.noreply.github.com>
2023-01-05 20:16:25 -0800

    working code for week 1

--- week1/script.js
+++ week1/script.js
@@ -1,4 +1,5 @@
 let firstName = "Emily";
 let lastName = "Brune";
-let yearsOfStudy = 1.5;
-let goal = "I'd like to be a Data Engineer or maybe get into data analytics. I want to work with Python and SQL"
+let yearsOfStudy = "1.5 years";
+let goal = "be a Data Engineer or maybe get into data analytics. I want to work with Python and SQL";
+let niceToMeetYou = "My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses.";
