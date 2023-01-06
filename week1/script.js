cb752008208c45a8c2f7835ba3e24a594eb4167c
Emily Brune <79616525+Emily-is-cool@users.noreply.github.com>
2023-01-06 03:06:10 +0000

    adding my first files for week 1 assignment

---
+++ week1/script.js
@@ -0,0 +1,4 @@
+let firstName = "Emily";
+let lastName = "Brune";
+let yearsOfStudy = 1.5;
+let goal = "I'd like to be a Data Engineer or maybe get into data analytics. I want to work with Python and SQL"
