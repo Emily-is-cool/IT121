898701f264e79b5b431e2b5a65df027abf6a8f32
Emily-is-cool <79616525+Emily-is-cool@users.noreply.github.com>
2023-01-14 19:41:50 -0800

    added comments

--- week2/pig.js
+++ week2/pig.js
@@ -1,13 +1,18 @@
+//Piglatin generator code by Emily Brune, January 14, 2023
+
 let word = prompt("Enter a word to find the piglatin translation.");
+//identify first and second letters
 let firstLetter = word.charAt(0);  
 let secondLetter = word.charAt(1);
 let vowels = "aeiouAEIOU" 
 let consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 //console.log(firstLetter);
 //console.log(secondLetter);
-
+//chop off the front of word types
 let newWord1 = word.slice(1);
 let newWord2 = word.slice(2);
+//checks before output
+//problem - somehow null value is in both vowels and consonants, length filter in place until I find a solution
 if (word.length < 1) {
     console.log("Warning, no input detected!");
 }
