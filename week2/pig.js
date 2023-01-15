c4d5de5fba0608de4972295e9d197e7f72806906
Emily-is-cool <79616525+Emily-is-cool@users.noreply.github.com>
2023-01-14 19:29:40 -0800

    new feature: null warning

--- week2/pig.js
+++ week2/pig.js
@@ -1,5 +1,5 @@
 let word = prompt("Enter a word to find the piglatin translation.");
-let firstLetter = word.slice(0, 1);  
+let firstLetter = word.charAt(0);  
 let secondLetter = word.charAt(1);
 let vowels = "aeiouAEIOU" 
 let consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
@@ -8,16 +8,16 @@ let consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 
 let newWord1 = word.slice(1);
 let newWord2 = word.slice(2);
-console.log(newWord1)
-console.log(newWord2);
+if (word.length < 1) {
+    console.log("Warning, no input detected!");
+}
 
-if (vowels.includes(firstLetter)) {
+if ((vowels.includes(firstLetter)) && (word.length > 1)) {
    console.log(word + "way");
 }
-else if ((consonants.includes(firstLetter)) && (consonants.includes(secondLetter))) {
+else if ((consonants.includes(firstLetter)) && (consonants.includes(secondLetter)) && (word.length > 1)) {
     console.log(word.slice(2) + firstLetter + secondLetter + "ay")
 }
-else if ((consonants.includes(firstLetter)) && (vowels.includes(secondLetter))) {
+else if ((consonants.includes(firstLetter)) && (vowels.includes(secondLetter)) && (word.length > 1)) {
     console.log(word.slice(1) + firstLetter + "ay"); 
 }
-
